9028db03c2096d4f9bc6b53968b2aeead1f5e57a
Mark Sayers <marksayers46@icloud.com>
2022-05-15 15:05:55 -0500

    setting up POST request

--- server.js
+++ server.js
@@ -9,5 +9,18 @@ const app = express()
 const URL = 'https://359b5ce3-d4c6-467c-a49a-85d9b3b9305e-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections'
 const token = 'AstraCS:RXZUowuqMmbBfXvBOlvPXPgZ:dc7248ef862c07e426391032b045e7d08296587e534223476bbe3ac4ee1a98c6'
 
+app.post('/tickets', async (req, res) => {
+    const formData = req.body.formData
+
+    const options = {
+        method: 'POST',
+        headers: {
+            Accepts: 'application/json',
+            'X-Cassandra-Token': token,
+            'Content-Type': 'application/json' 
+        },
+        data: formData
+    }
+})
 
 app.listen(PORT, () => console.log('server running on PORT ' + PORT))
