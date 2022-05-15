5f254ba293211078f77a9be55cf192a975fe5821
Mark Sayers <marksayers46@icloud.com>
2022-05-14 19:15:38 -0500

    setting up express server

---
+++ server.js
@@ -0,0 +1,9 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+
+const app = express()
+
+app.listen(PORT, () => console.log('server running on PORT ' + PORT))
