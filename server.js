43994e9494b80061c387b2f860d0772e0e76e30a
Mark Sayers <marksayers46@icloud.com>
2022-05-17 12:40:17 -0500

    setting up GET rquest

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ app.use(express.json())
 const url = 'https://e7994b8f-d826-44c5-a960-6f030f85bb51-southcentralus.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections/tasks'
 const token = 'AstraCS:HwQithiZmequOLQnGnRRTtrr:7ecc7390f3acd47a308202ae7b8a330ec761f96ccfaf831ea2166453f3dd86c1'
 
+
+app.get('/tickets', async ( req, res) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            Accepts: 'application/json',
+            'X-Cassandra-Token': token,
+        }
+    }
+    try {
+        const response = await axios(`${url}?page-size=20`, options)
+        res.status(200).json(response.data)
+    }catch (err) {
+        console.log(err)
+        res.status(500).json({message: err})
+    }
+})
+
+
 app.post('/tickets', async (req, res) => {
     const formData = req.body.formData
 
