3669a43addf79a6ad0c453404f072a1c57fa4af4
Mark Sayers <marksayers46@icloud.com>
2022-07-29 11:45:45 -0500

    added dummy data hardcoded to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,60 +10,60 @@ const colors = [
 ]
 
 // dummy data
-// const tickets = [
-//   {
-//     category: 'Q1 2022',
-//     color: "blue",
-//     title: 'NFT Safety 101 Video',
-//     owner: 'Mark Sayers',
-//     avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
-//     status: 'done',
-//     priority: 5,
-//     progress: 100,
-//     description:
-//       'Make a video showcasing how to work with NFTs safely, including how to know if one is not genuine.',
-//     timestamp: '2022-02-11T07:36:17+0000',
-//   },
-//   {
-//     category: 'Q1 2022',
-//     color: 'red',
-//     title: 'Build and Sell an AI Model',
-//     owner: 'Mark Sayers',
-//     avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
-//     status: 'working on it',
-//     priority: 4,
-//     progress: 70,
-//     description:
-//       'Show how easy it is to work with machine Learning and show people how to make money by building and selling an AI API',
-//     timestamp: '2022-02-13T07:36:17+0000',
-//   },
-//   {
-//     category: 'Q2 2022',
-//     color: 'red',
-//     title: 'My top tools for 2022',
-//     owner: 'Mark Sayers',
-//     avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
-//     status: 'stuck',
-//     priority: 3,
-//     progress: 35,
-//     description:
-//       'Share my top tools for 2022 with everyone in a 15 minute video.',
-//     timestamp: '2022-02-16T07:36:17+0000',
-//   },
-//   {
-//     category: 'Q2 2022',
-//     color: 'blue',
-//     title: 'Frogger in React',
-//     owner: 'Mark Sayers',
-//     avatar: null,
-//     status: '',
-//     priority: 0,
-//     progress: 0,
-//     description:
-//       'Make a video showing how to build the popular retro game Frogger in React.',
-//     timestamp: '2022-02-21T07:36:17+0000',
-//   },
-// ]
+const tickets = [
+  {
+    category: 'Q1 2022',
+    color: "blue",
+    title: 'Team-Manager',
+    owner: 'Mark Sayers',
+    avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
+    status: 'done',
+    priority: 5,
+    progress: 100,
+    description:
+      'Control team projects & check/update status',
+    timestamp: '2022-02-11T07:36:17+0000',
+  },
+  {
+    category: 'Q1 2022',
+    color: 'red',
+    title: 'React Grocery List',
+    owner: 'Mark Sayers',
+    avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
+    status: 'stuck',
+    priority: 4,
+    progress: 70,
+    description:
+      'Not your every day todo list app, built to be more dynamically UI/UX friendly.',
+    timestamp: '2022-02-13T07:36:17+0000',
+  },
+  {
+    category: 'Q2 2022',
+    color: 'red',
+    title: 'Delicious',
+    owner: 'Mark Sayers',
+    avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
+    status: 'working on it',
+    priority: 3,
+    progress: 80,
+    description:
+      'More than just a recipe app! We provide solutions to make any food-related vision a reality.',
+    timestamp: '2022-02-16T07:36:17+0000',
+  },
+  {
+    category: 'Q2 2022',
+    color: 'blue',
+    title: 'Personal Portfolio',
+    owner: 'Mark Sayers',
+    avatar: null,
+    status: '',
+    priority: 0,
+    progress: 100,
+    description:
+      'Portfolio built with JavaScript/jQuery.',
+    timestamp: '2022-02-21T07:36:17+0000',
+  },
+]
 
 // Set object lets us store unique property values once from our above hard coded data
 const uniqueCategories = [
