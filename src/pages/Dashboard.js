3d059c3c4d5249febc8843d5ece5cb697e06fb0c
Mark Sayers <marksayers46@icloud.com>
2022-08-01 17:01:49 -0500

    setting up dashboard state

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,77 +1,46 @@
 import TicketCard from '../components/TicketCard'
+import axios from 'axios'
+import { useState, useEffect, useContext } from "react"
 
-
-const colors = [
-  'rgb(186,255,201)',
-  'rgb(255,223,186)',
-  'rgb(186,255,255)',
-  'rgb(255,179,186)',
-  'rgb(255,255,186)',
-]
-
-// hardcoded dummy data
-const tickets = [
-  {
-    category: 'Q1 2022',
-    color: "blue",
-    title: 'Team-Manager',
-    owner: 'Mark Sayers',
-    avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
-    status: 'done',
-    priority: 2,
-    progress: 100,
-    description:
-      'Control team projects & check/update status',
-    timestamp: '2022-02-11T07:36:17+0000',
-  },
-  {
-    category: 'Q1 2022',
-    color: 'red',
-    title: 'React Grocery List',
-    owner: 'Mark Sayers',
-    avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
-    status: 'stuck',
-    priority: 4,
-    progress: 55,
-    description:
-      'Not your every day todo list app, built to be more dynamically UI/UX friendly.',
-    timestamp: '2022-02-13T07:36:17+0000',
-  },
-  {
-    category: 'Q2 2022',
-    color: 'red',
-    title: 'Delicious',
-    owner: 'Mark Sayers',
-    avatar: 'https://avatars.githubusercontent.com/u/50345632?v=4',
-    status: 'working on it',
-    priority: 3,
-    progress: 80,
-    description:
-      'More than just a recipe app! We provide solutions to make any food-related vision a reality.',
-    timestamp: '2022-02-16T07:36:17+0000',
-  },
-  {
-    category: 'Q2 2022',
-    color: 'blue',
-    title: 'Personal Portfolio',
-    owner: 'Mark Sayers',
-    avatar: null,
-    status: '',
-    priority: 5,
-    progress: 100,
-    description:
-      'Portfolio built with JavaScript/jQuery.',
-    timestamp: '2022-02-21T07:36:17+0000',
-  },
-]
-
-// Set object lets us store unique property values once from our above hard coded data
-const uniqueCategories = [
-  ...new Set(tickets?.map(({ category }) => category))
-]
-console.log("Testing Categories:", uniqueCategories)
 
 const Dashboard = () => {
+  const [tickets, setTickets] = useState(null)
+
+  useEffect(async () => {
+    const response = await axios.get('http://localhost:8000/tickets')
+
+    const dataObject = response.data.data
+
+    // taking returned object and returning it in an array
+    const arrayOfKeys = Object.keys(dataObject)
+    const arrayOfData = Object.keys(dataObject).map((key) => dataObject[key])
+    // console.log('arrayOfKeys:', arrayOfKeys)
+    // console.log('arrayOfData:', arrayOfData)
+
+    // creating a new array 'formattedArray' so we can re-format the returned array of objects
+    const formattedArray = []
+    arrayOfKeys.forEach((key, index) => {
+      const formattedData = { ...arrayOfData[index]} 
+      formattedData['documentId'] = key
+      formattedArray.push(formattedData)
+      // console.log('formattedArray:', formattedArray)
+    })
+
+  }, [])
+
+  const colors = [
+    'rgb(186,255,201)',
+    'rgb(255,223,186)',
+    'rgb(186,255,255)',
+    'rgb(255,179,186)',
+    'rgb(255,255,186)',
+  ]
+  
+  // Set object lets us store unique property values once from our data
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category))
+  ]
+  console.log("Testing Categories:", uniqueCategories)
 
   return (
     <div className="dashboard">
